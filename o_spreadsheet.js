4a07ec856c78d7d0f170c52e060b88fd668bcf49
Adrien Minne (adrm) <adrm@odoo.com>
2023-02-23 08:18:40 +0100

    fetch both 9000 & 9090 for branches that aren't yet rebased

--- o_spreadsheet.js
+++ o_spreadsheet.js
@@ -4,6 +4,7 @@ console.log("O-Spreadsheet Chrome Extension Loaded");
 window.addEventListener("keydown", (e) => {
     if (e.key === "m" && e.ctrlKey) {
         fetch("http://localhost:9090/clear");
+        fetch("http://localhost:9000/clear");
         location.reload();
     }
 });
